3ead89ae9912faac6a33d40bcbfc8bade089955f
Swapnilthakare86 <sthakare@xtsworld.in>
2026-02-12 15:29:46 +0530

    Updated main

--- StudentRecordManager/main.py
+++ StudentRecordManager/main.py
@@ -1,52 +1,36 @@
-
-from operation import (addStudent,viewStudets,updateStudent,deleteStudent,searchStudent,init_file)
+from operation import ( addStudent, viewStudents, updateStudent, deleteStudent,searchStudent,init_file)
 
 def main():
-    
     init_file()
 
-    while(True):
-        print("Student Record Manager")
-        print("1.Add Student Record")
-        print("2.View Student Records")
-        print("3.Update Student Record")
-        print("4.Delete Student Record")
-        print("5.Search Student Record")
-        print("6.Exit")
+    while True:
+        print("\n===== Student Record Manager =====")
+        print("1. Add Student Record")
+        print("2. View Student Records")
+        print("3. Update Student Record")
+        print("4. Delete Student Record")
+        print("5. Search Student Record")
+        print("6. Exit")
 
-        choice=input("Enter Your Choice:")
+        choice = input("Enter Your Choice: ")
 
-        if choice==1:
+
+        if choice == "1":
             addStudent()
-
-        elif choice==2:
-            viewStudets()
- 
-        elif choice==3:
+        elif choice == "2":
+            viewStudents()
+        elif choice == "3":
             updateStudent()
-
-        elif choice==4:
+        elif choice == "4":
             deleteStudent()
-
-        elif choice==5:
+        elif choice == "5":
             searchStudent()
-
-        elif choice==6:
-            print("Exit program.")
+        elif choice == "6":
+            print("Exiting program...")
             break
-        
         else:
             print("Invalid Choice.")
 
-if __name__=="__main__":
+
+if __name__ == "__main__":
     main()
-
-            
-            
-
-       
-
-       
-       
-
-
