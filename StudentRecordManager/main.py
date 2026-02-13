e303b7e5903e67a2d624fed90d718a862e0d9485
Swapnilthakare86 <sthakare@xtsworld.in>
2026-02-13 15:45:19 +0530

    Update main.py

--- StudentRecordManager/main.py
+++ StudentRecordManager/main.py
@@ -1,4 +1,4 @@
-from operation import ( addStudent, viewStudents, updateStudent, deleteStudent,searchStudent,init_file)
+from operation import ( addStudent, viewStudents, updateStudent, deleteStudent,searchStudent,init_file,filterByGrade)
 
 def main():
     init_file()
@@ -10,7 +10,8 @@ def main():
         print("3. Update Student Record")
         print("4. Delete Student Record")
         print("5. Search Student Record")
-        print("6. Exit")
+        print("6.Search Student ByGrade")
+        print("7. Exit")
 
         choice = input("Enter Your Choice: ")
 
@@ -25,11 +26,16 @@ def main():
             deleteStudent()
         elif choice == "5":
             searchStudent()
-        elif choice == "6":
+        elif choice =="6":
+            filterByGrade()
+        elif choice == "7":
             print("Exiting program...")
             break
         else:
             print("Invalid Choice.")
+
+        
+
 
 
 if __name__ == "__main__":
